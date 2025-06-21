aa07fc20a869d8afe266c4711feb653d4379fd97
西西 <2895105284@qq.com>
2025-06-21 16:44:11 +0800

    Java实现的简单跳表

--- src/SkipListNode.java
+++ src/SkipListNode.java
@@ -1,6 +1,10 @@
+/**
+ * 跳表节点
+ * @param <T>
+ */
 class SkipListNode<T extends Comparable<T>>{
-    public T value;
-    public SkipListNode<T>[] next;
+    public T value;//节点的值
+    public SkipListNode<T>[] next;//节点每一层的下一个节点
 
     public SkipListNode(T value,int level){
         this.value=value;
